ee16494294fc9a27c0336a60aba519956352d1f0
Adam Jayne <adamjayne402@gmail.com>
2018-02-14 15:59:09 -0500

    added access to nasa's api to add to the MISC page. Included Nasa's picture of the day.

--- controller/misc-controller.js
+++ controller/misc-controller.js
@@ -1,3 +1,18 @@
-angular.module('myPort').controller('ExtraController', ['$scope', function($scope){
+angular.module('myPort').controller('ExtraController', ['$scope', '$http', function($scope, $http){
+    var nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=0YB4fW2DCUngcV4IcsSYY9Y3wBtJKdCtmzHzU2sC"
+    var apiKey = "0YB4fW2DCUngcV4IcsSYY9Y3wBtJKdCtmzHzU2sC"
+    var apod = "https://api.nasa.gov/planetary/apod";
+    $http({
+        url: apod,
+        method: 'GET',
+        params: {
+            api_key: apiKey,
+            hd: true,
+        }
+    }).then(function(response){
+        $scope.imageOfTheDay = response.data.hdurl;
+    }, function(err){
+        console.log(err);
+    })
     console.log('Extra Page');
 }])
